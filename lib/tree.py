ad47f50be9462ee0518c748092332760c8c8b0d0
Gabrielle <ms.grodarte@gmail.com>
2025-01-27 11:56:20 -0800

    complete lab

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,38 @@
+import ipdb
+
 class Tree:
   def __init__(self, root = None):
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    nodes_to_visit = [self.root]
+
+    while len(nodes_to_visit) > 0:
+      node = nodes_to_visit.pop(0)
+      if node['id'] == id:
+        return node
+      else:
+        nodes_to_visit = node['children'] + nodes_to_visit
+    return None
+
+  def breadth_first_traversal(node):
+    result = []
+    nodes_to_visit = [node]
+
+    while len(nodes_to_visit) > 0:
+      node = nodes_to_visit.pop(0)
+      result.append(node['value'])
+      nodes_to_visit = nodes_to_visit + node['children']
+
+    return result
+
+  def depth_first_traversal(node):
+    result = []
+    nodes_to_visit = [node]
+
+    while len(nodes_to_visit) > 0:
+      node = nodes_to_visit.pop(0)
+      result.append(node['value'])
+      nodes_to_visit = node['children'] + nodes_to_visit
+
+    return result
